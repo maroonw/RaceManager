6073e4a58a57dcf22b233df738a1bdfb894b65dc
w-mccracken <wmmccrac@asu.edu>
2025-10-07 18:14:24 -0400

    Update RacerSubscriber.java

--- RaceManager/src/model/notification/RacerSubscriber.java
+++ RaceManager/src/model/notification/RacerSubscriber.java
@@ -2,15 +2,17 @@ package model.notification;
 
 import model.Racer;
 
+// Racers who are subscribed to receive notifications
 public class RacerSubscriber implements IObserver {
-    private String racerName;
-    private Racer racer;
+    private final String racerName;
+    private final Racer racer;
 
     public RacerSubscriber(Racer racer) {
         this.racerName = racer.getName();
         this.racer = racer;
     }
 
+    // Update to notify global or race-scoped subscribers (observers) of an event
     @Override
     public void update(String eventType, String data) {
         racer.receiveNotification("Event Type: " + eventType + "\nData: " + data + "\n");
